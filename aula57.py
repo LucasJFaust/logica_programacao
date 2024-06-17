408a6466b5c8cd1d2848745afa274aa396c712fa
LucasJFaust <lucasjfm.eng@gmail.com>
2024-06-17 14:15:21 -0300

    aulas 57 e 58

---
+++ aula57.py
@@ -0,0 +1,16 @@
+"""
+Listas Dentro de listas e seus índices
+"""
+#..........0.........1
+salas = [['Maria', 'Helena'], # 0
+#...........0
+         ['Elaine', ], # 1
+#..........0.........1.......2
+         ['Luiz', 'João', 'Eduarda', (0, 10, 20, 30, 40)]] # 2
+
+# print(salas[2][3][2])
+
+for sala in salas:
+    print(f'A sala é {sala}')
+    for aluno in sala:
+        print(aluno)
